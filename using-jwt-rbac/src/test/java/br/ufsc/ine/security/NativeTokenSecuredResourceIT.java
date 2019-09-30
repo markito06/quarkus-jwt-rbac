60c4f3487a8e3569b120323de8e872670f9eaf42
Marcos Alexandre V. Costa <marcos.avc@gmail.com>
2019-09-30 20:27:18 -0300

    first commit

---
+++ using-jwt-rbac/src/test/java/br/ufsc/ine/security/NativeTokenSecuredResourceIT.java
@@ -0,0 +1,9 @@
+package br.ufsc.ine.security;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeTokenSecuredResourceIT extends TokenSecuredResourceTest {
+
+    // Execute the same tests but in native mode.
+}
